e15b4bf3ea8eaa1ddda517bf26665c4d7facb01d
Aliaksandr Tserashkou <ter.alexanderandr@gmail.com>
2024-03-03 13:56:38 +0100

    Network request example

--- tca-essentials/tca-essentials/CounterReducer.swift
+++ tca-essentials/tca-essentials/CounterReducer.swift
@@ -1,4 +1,5 @@
 import ComposableArchitecture
+import Foundation
 
 @Reducer
 struct CounterReducer {
@@ -6,11 +7,15 @@ struct CounterReducer {
     @ObservableState
     struct State {
         var count = 0
+        var fact: String?
+        var isLoading = false
     }
 
     enum Action {
         case decrementButtonTapped
         case incrementButtonTapped
+        case factButtonTapped
+        case factResponse(String)
     }
 
     var body: some ReducerOf<Self> {
@@ -18,9 +23,24 @@ struct CounterReducer {
             switch action {
             case .decrementButtonTapped:
                 state.count -= 1
+                state.fact = nil
                 return .none
             case .incrementButtonTapped:
                 state.count += 1
+                state.fact = nil
+                return .none
+            case .factButtonTapped:
+                state.fact = nil
+                state.isLoading = true
+                return .run { [count = state.count] send in
+                    let (data, _) = try await URLSession.shared
+                        .data(from: URL(string: "http://numbersapi.com/\(count)")!)
+                    let fact = String(decoding: data, as: UTF8.self)
+                    await send(.factResponse(fact))
+                }
+            case .factResponse(let fact):
+                state.fact = fact
+                state.isLoading = false
                 return .none
             }
         }
