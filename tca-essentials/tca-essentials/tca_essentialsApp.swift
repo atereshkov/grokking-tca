7e296475707255da93dd036094ebf2a06aca96ca
Aliaksandr Tserashkou <ter.alexanderandr@gmail.com>
2024-03-03 09:51:48 +0100

    Basic app files

---
+++ tca-essentials/tca-essentials/tca_essentialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  tca_essentialsApp.swift
+//  tca-essentials
+//
+//  Created by Aliaksandr Tserashkou on 03/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct tca_essentialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
